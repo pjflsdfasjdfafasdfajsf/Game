061c279f7b64d2aea2c62b2297b27d43ab641b60
pjflsdfasjdfafasdfajsf <peppohappy8@gmail.com>
2026-06-22 22:20:17 +0300

    Build script refactor

--- Build.sh
+++ Build.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 #
 # NOTE: Linux compilation script.
 # 
+set -e
+
 Compiler="clang"
 CommonCompilerFlags="-ICode/Public"
 
@@ -11,36 +14,78 @@ LinkerFlags="${VendorLibs} ${SystemLibs}"
 
 # NOTE: Dirs.
 
+AssetsDir="Assets"
 BuildDir="Build"
-BinDir="${BuildDir}/Bin"
+ToolsDir="${BuildDir}/Tools"
 
 mkdir -p ${BuildDir} \
-         ${BinDir}
+         ${ToolsDir}
+
+# NOTE: This is for printing.
+Align="%-15s"
+
+#
+# NOTE: Atlas
+# 
+
+AtlasPackSrc="Code/Host/AtlasPack.c \
+              Code/Host/STB.c
+             "
+AtlasPackTarget="${ToolsDir}/AtlasPack"
+AtlasPackFlags="${CommonCompilerFlags} -IExt/STB -O3 -march=native"
+AtlasPackLinkerFlags="-lm"
+
+GameAtlas="${BuildDir}/GameAtlas"
+
+ShouldPack=0
+
+if [ "$1" = "Pack" ]; then
+    ShouldPack=1
+fi
+
+# NOTE: If the atlas does not exist we do not care about what user passed in and
+# we should create it
+if [ ! -f ${AtlasPackTarget} ] || [ ! -f ${GameAtlas}.png ]; then
+    ShouldPack=1
+fi
+
+if [ ${ShouldPack} -eq 1 ]; then
+    printf ${Align} "AtlasPack"
+
+    ${Compiler} ${AtlasPackFlags} ${AtlasPackSrc} ${AtlasPackLinkerFlags} -o ${AtlasPackTarget}
+    printf "Done\n"
+
+    printf ${Align} "Packing"
+    ${AtlasPackTarget} ${GameAtlas} ${AssetsDir}/Images/*
+    printf "Done\n"
+fi
 
 #
 # NOTE: Host
 #
 
+printf ${Align} "Host"
 HostSrc="Code/Host/Main.c    \
          Code/Host/Runtime.c \
          Code/Host/STB.c     \
          Code/Host/SDL.c
         "
-HostTarget="${BinDir}/Host"
+HostTarget="${BuildDir}/Host"
 HostFlags="${CommonCompilerFlags} -IExt/WAMR/Include -IExt/SDL3/Include -IExt/STB"
 
 ${Compiler} ${HostFlags} ${HostSrc} ${LinkerFlags} -o ${HostTarget}
+printf "Done\n"
 
 #
 # NOTE: Game
 #
 
+printf ${Align} "Game"
 GameSrc="Code/Game/Game.c   \
          Code/Public/Init.c
         "
-GameTarget="${BinDir}/Game.wasm"
+GameTarget="${BuildDir}/Game.wasm"
 GameFlags="${CommonCompilerFlags} --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined"
 
 ${Compiler} ${GameFlags} ${GameSrc} -o ${GameTarget}
-
-echo "Done"
+printf "Done\n"
